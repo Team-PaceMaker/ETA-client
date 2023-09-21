d27b162e1b10467be3176316cd3bbe1ad3ab24b4
rbgksqkr <rbgks1937@naver.com>
2023-09-21 15:33:34 +0900

    Feat: #1 - 노트북 카메라로 촬영

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./home.module.css";
+
+const constraints = { audio: false, video: true };
 
 function Home() {
+  const [isVideo, setIsVideo] = useState(false);
+
+  const handleClickRecord = () => {
+    setIsVideo((prev) => !prev);
+    navigator.mediaDevices.getUserMedia(constraints).then(function (mediaStream) {
+      // 비디오 트랙을 포함한 MediaStream
+      console.log(mediaStream);
+      const videoOutput = document.getElementById("video-output");
+      if (videoOutput instanceof HTMLVideoElement) {
+        videoOutput.srcObject = mediaStream;
+        // metadata가 로드될 때 실행되는 이벤트
+        videoOutput.onloadedmetadata = function () {
+          videoOutput.play();
+        };
+      }
+    });
+  };
+
   return (
     <React.Fragment>
       <Head>
-        <title>Home - Nextron (with-typescript)</title>
+        <title>ETA(Encourage Time for Attention)</title>
       </Head>
       <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ -
-          <Link href="/next">
-            <a>Go to next page</a>
-          </Link>
-        </p>
-        <Image src="/images/logo.png" alt="Logo image" width={100} height={100} />
+        {isVideo ? (
+          <video id="video-output" className={styles.video}></video>
+        ) : (
+          <div style={{ width: 200, height: 200, backgroundColor: "red" }}></div>
+        )}
+        <button style={{ display: "block" }} onClick={handleClickRecord}>
+          카메라 촬영
+        </button>
       </div>
     </React.Fragment>
   );
