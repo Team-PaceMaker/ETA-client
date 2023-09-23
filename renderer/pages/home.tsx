34b9468d96b4ee9c448f896040c13f7cc2970cf0
rbgksqkr <rbgks1937@naver.com>
2023-09-24 00:42:43 +0900

    Feat: #4 - 푸시알림 기능 구현

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -26,6 +26,31 @@ function Home() {
     });
   };
 
+  const showNotification = () => {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      // TODO: 집중상태를 값으로 받아서 값에 따라 푸시알림 보내기
+      const notificationTitle = "현재 집중상태 : 🔥";
+      new Notification(notificationTitle, {
+        body: "오~쫌치는데? 아자아자!",
+      }).onclick = () => console.log("Notification Clicked");
+      // const notificationTitle = "현재 집중상태 : 🫵";
+      // new Notification(notificationTitle, {
+      //   body: "엥 지금 집중안하고 뭐함?",
+      // }).onclick = () => console.log("Notification Clicked");
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission((permission) => {
+        console.log(permission);
+        if (permission === "granted") {
+          new Notification("START ETA", { body: "이제 푸시알림을 받을 수 있습니다." });
+        } else {
+          alert("Notification denied");
+        }
+      });
+    }
+  };
+
   return (
     <React.Fragment>
       <Head>
@@ -39,6 +64,10 @@ function Home() {
         )}
         <Button style={{ display: "block" }} onClick={handleClickRecord} variant="contained">
           카메라 촬영
+        </Button>
+
+        <Button style={{ display: "block" }} onClick={showNotification} variant="contained">
+          푸시 알림
         </Button>
       </div>
       <PieChart />
