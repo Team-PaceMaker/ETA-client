c26328e451827a3ef956544251f12b070b6058e2
rbgksqkr <rbgks1937@naver.com>
2023-09-26 14:02:14 +0900

    Feat: #6 - Header 컴포넌트 이벤트 추가

--- renderer/components/common/Header.tsx
+++ renderer/components/common/Header.tsx
@@ -1,11 +1,19 @@
+import { useRouter } from "next/router";
 import React from "react";
+import FONT from "../../constants/fonts";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <div className={styles.headerContainer}>
-      <div>로고</div>
-      <div>로그인</div>
+    <div className={styles.headerContainer} style={FONT.BODY1}>
+      <div className={styles.logo} onClick={() => router.push("/home")}>
+        로고
+      </div>
+      <div className={styles.login} onClick={() => router.push("/login")}>
+        로그인
+      </div>
     </div>
   );
 };
