4af966fe23922d869557ef411584f1e8a6e92d82
rbgksqkr <rbgks1937@naver.com>
2023-09-26 14:02:33 +0900

    Design: #6 - font 파일 분리

---
+++ renderer/constants/fonts.ts
@@ -0,0 +1,26 @@
+import { CSSProperties } from "react";
+
+const SLOGAN: CSSProperties = {
+  fontSize: "28px",
+  fontWeight: "600",
+  color: "white",
+};
+
+const HEADLINE1: CSSProperties = {
+  fontSize: "24px",
+  fontWeight: "600",
+  color: "white",
+};
+
+const BODY1: CSSProperties = {
+  fontSize: "16px",
+  fontWeight: "600",
+};
+
+const FONT = {
+  SLOGAN,
+  HEADLINE1,
+  BODY1,
+};
+
+export default FONT;
