45130bb5d83f1061db722a955b0ea225c8100ac1
rbgksqkr <rbgks1937@naver.com>
2023-09-26 13:28:52 +0900

    Feat: #6 - 공통 layout 설정

---
+++ renderer/pages/RootLayout.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+import Header from "../components/common/Header";
+import styles from "./home.module.css";
+
+const RootLayout = ({ children }) => {
+  return (
+    <div className={styles.layout}>
+      <Head>
+        <title>ETA(Encourage Time for Attention)</title>
+      </Head>
+      <Header />
+      <>{children}</>
+    </div>
+  );
+};
+
+export default RootLayout;
