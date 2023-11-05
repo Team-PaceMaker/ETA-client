9acc8fa23cd96db9eec7ea18b0115c856a0577ce
rbgksqkr <rbgks1937@naver.com>
2023-11-05 22:01:06 +0900

    feat: 이미지를 formData로 보내면 집중 상태 반환

--- renderer/pages/camera/index.tsx
+++ renderer/pages/camera/index.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import styles from "./camera.module.css";
-import RootLayout from "../RootLayout";
-import useInterval from "../../hooks/useInterval";
-import FONT from "../../constants/fonts";
-import { sendCaptureImage } from "../../apis/camera";
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import styles from './camera.module.css';
+import RootLayout from '../RootLayout';
+import useInterval from '../../hooks/useInterval';
+import FONT from '../../constants/fonts';
+import { sendCaptureImage } from '../../apis/camera';
 
 const constraints = { audio: false, video: true };
 const VIDEO_WIDTH = 500;
@@ -13,7 +13,7 @@ const CAPTURE_DELAY = 1000;
 const IMAGE_WIDTH = 224;
 const IMAGE_HEIGHT = 224;
 
-const BUTTON_TEXT = "VIDEO START";
+const BUTTON_TEXT = 'VIDEO START';
 let scaleFactor = 0.25;
 let videoStream: MediaStream;
 
@@ -24,10 +24,10 @@ const CameraGuidePage = () => {
     }
     var w = video.videoWidth * scaleFactor;
     var h = video.videoHeight * scaleFactor;
-    var canvas = document.createElement("canvas");
+    var canvas = document.createElement('canvas');
     canvas.width = w;
     canvas.height = h;
-    var ctx = canvas.getContext("2d");
+    var ctx = canvas.getContext('2d');
     ctx.scale(-1, 1);
     ctx.translate(-w, 0);
     ctx.drawImage(video, 0, 0, w, h);
@@ -36,24 +36,24 @@ const CameraGuidePage = () => {
   };
 
   const captureImage = () => {
-    const video = document.getElementById("video-output") as HTMLVideoElement;
-    const output = document.getElementById("output");
+    const video = document.getElementById('video-output') as HTMLVideoElement;
+    const output = document.getElementById('output');
     const canvas = capture(video, scaleFactor);
     if (canvas.width === 0) return;
-    const imageSrc = canvas.toDataURL("image/jpeg", 0.8); // 2번째 인자를 0~1 까지 주면서 화질 조절. 1이 best
+    const imageSrc = canvas.toDataURL('image/jpeg', 0.8); // 2번째 인자를 0~1 까지 주면서 화질 조절. 1이 best
 
     // Base64 문자열을 Blob으로 변환
     const blob = dataURLtoBlob(imageSrc);
 
     // Blob 데이터를 FormData에 담아 송신
     const formData = new FormData();
-    formData.append("file", blob, "test.jpeg");
+    formData.append('image', blob, 'test.jpeg');
     sendCaptureImage(formData);
   };
 
   const showCameraGuide = () => {
     navigator.mediaDevices.getUserMedia(constraints).then(function (mediaStream) {
-      const videoOutput = document.getElementById("video-output");
+      const videoOutput = document.getElementById('video-output');
       if (videoOutput instanceof HTMLVideoElement) {
         videoOutput.srcObject = mediaStream;
         videoStream = mediaStream;
@@ -66,7 +66,7 @@ const CameraGuidePage = () => {
   };
 
   function stopVideoStream() {
-    const videoOutput = document.getElementById("video-output");
+    const videoOutput = document.getElementById('video-output');
     if (videoOutput instanceof HTMLVideoElement) {
       const tracks = videoStream.getTracks(); // 스트림의 모든 트랙 가져오기
       tracks.forEach(function (track) {
@@ -81,7 +81,7 @@ const CameraGuidePage = () => {
   // }, CAPTURE_DELAY);
 
   useEffect(() => {
-    if ("navigator" in window) {
+    if ('navigator' in window) {
       showCameraGuide();
     }
     return () => stopVideoStream();
@@ -91,12 +91,12 @@ const CameraGuidePage = () => {
     <RootLayout>
       <div className={styles.cameraBodyContainer}>
         <video
-          id="video-output"
+          id='video-output'
           width={VIDEO_WIDTH}
           height={VIDEO_HEIGHT}
           className={styles.video}
         ></video>
-        <Link href="/video">
+        <Link href='/video'>
           <div className={styles.buttonContainer} style={FONT.BODY1}>
             {BUTTON_TEXT}
           </div>
@@ -112,8 +112,8 @@ const CameraGuidePage = () => {
 
 // Base64 문자열을 Blob으로 변환하는 함수
 function dataURLtoBlob(dataURL: string) {
-  const byteString = atob(dataURL.split(",")[1]);
-  const mimeString = dataURL.split(",")[0].split(":")[1].split(";")[0];
+  const byteString = atob(dataURL.split(',')[1]);
+  const mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0];
   const ab = new ArrayBuffer(byteString.length);
   const ia = new Uint8Array(ab);
   for (let i = 0; i < byteString.length; i++) {
