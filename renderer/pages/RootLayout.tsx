a8d55edadf0b2a311135f673791d53c14932a1d0
rbgksqkr <rbgks1937@naver.com>
2023-11-16 22:33:08 +0900

    chore: 보안정책 및 메타태그 추가(#22)

--- renderer/pages/RootLayout.tsx
+++ renderer/pages/RootLayout.tsx
@@ -8,6 +8,10 @@ const RootLayout = ({ children }) => {
     <>
       <Head>
         <title>ETA(Encourage Time for Attention)</title>
+        <meta
+          name='description'
+          content={'장시간 노트북 작업하는 현대인을 위한 딥러닝 기반 집중력 관리 서비스'}
+        />
       </Head>
       <Header />
       <div className={styles.rootLayout}>{children}</div>
