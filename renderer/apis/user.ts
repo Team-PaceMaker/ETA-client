400377a55edd2237d8d06a0c187705f5e98fd3f8
rbgksqkr <rbgks1937@naver.com>
2023-12-01 16:41:50 +0900

    feat: 유저 정보 API 구현(#28)

--- renderer/apis/user.ts
+++ renderer/apis/user.ts
@@ -1,5 +1,23 @@
 import { defaultServer } from './settings';
 
+interface ILoginToken {
+  grantType: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
 export const login = async (authCode: string) => {
-  const result = await defaultServer.post('api/v1/auth/kakao/login', { authCode });
+  const result = await defaultServer.post<ILoginToken>('api/v1/auth/kakao/login', { authCode });
+  // token 값 로컬 스토리지에 저장
+  localStorage.setItem('accessToken', result.data.accessToken);
+  localStorage.setItem('refreshToken', result.data.refreshToken);
+};
+
+export const getUserInfo = async () => {
+  const result = await defaultServer.get('api/v1/auth/kakao/info');
+};
+
+export const logout = async () => {
+  const result = await defaultServer.get('api/v1/auth/kakao/logout');
+  localStorage.clear();
 };
